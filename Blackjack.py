80dac3b9bb6278189f80ce5f38171ade972f7329
Glauco da Silva <glaucodasilva@live.com>
2018-12-04 21:39:23 -0200

    Criação de baralho e impressão de cartas.

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,30 @@
+class sal:
+    def __init__(self, saldo = 1000):
+        self.saldo = saldo
+
+    def val(self):
+        return self.saldo
+
+import random
+
 def inicio():
+    saldo = sal()
+    print('                                           Bem Vindo ao Blackjack!                                           ')
+    print('')
+    print('Para iniciar a partida digite o valor de sua aposta entre 1 e 25, são permitidos apenas números inteiteiros!')
+    print('')
+    print('O seu saldo atual é: ', saldo.val())
+
+def criarbaralho():
+    baralho = list(range(1, 14))*30
+    random.shuffle(baralho)
+    return baralho
+
+def controlasaldo():
     pass
 def jogadores():
+    pass
+def creditoinicial():
     pass
 def vencedor():
     pass
@@ -10,8 +34,20 @@ def imprimecartas():
 
     carta2 = ['############', '#          #', '#  ######  #', '#       #  #', '#       #  #', '#  ######  #', '#  #       #', '#  #       #', '#  ######  #', '#          #', '############']
     carta3 = ['############', '#          #', '#  ######  #', '#       #  #', '#       #  #', '#  ######  #', '#       #  #', '#       #  #', '#  ######  #', '#          #', '############']
-    cartas = [carta2, carta3]
-    qtcartas = 2
+    carta4 = ['############', '#          #', '#  #    #  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#       #  #', '#       #  #', '#       #  #', '#          #', '############']
+    carta5 = ['############', '#          #', '#  ######  #', '#  #       #', '#  #       #', '#  ######  #', '#       #  #', '#       #  #', '#  ######  #', '#          #', '############']
+    carta6 = ['############', '#          #', '#  ######  #', '#  #       #', '#  #       #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#          #', '############']
+    carta7 = ['############', '#          #', '#  ######  #', '#       #  #', '#       #  #', '#       #  #', '#       #  #', '#       #  #', '#       #  #', '#          #', '############']
+    carta8 = ['############', '#          #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#          #', '############']
+    carta9 = ['############', '#          #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#       #  #', '#       #  #', '#       #  #', '#          #', '############']
+    carta10 = ['############', '#          #', '# # ###### #', '# # #    # #', '# # #    # #', '# # #    # #', '# # #    # #', '# # #    # #', '# # ###### #', '#          #', '############']
+    cartaJ = ['############', '#          #', '#       #  #', '#       #  #', '#       #  #', '#       #  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#          #', '############']
+    cartaQ = ['############', '#          #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  #    #  #', '#  #  # #  #', '#  #   ##  #', '#  ######  #', '#        # #', '############']
+    cartaK = ['############', '#          #', '#  #    #  #', '#  #   #   #', '#  # #     #', '#  #####   #', '#  #    #  #', '#  #    #  #', '#  #    #  #', '#          #', '############']
+    cartaA = ['############', '#          #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  ######  #', '#  #    #  #', '#  #    #  #', '#  #    #  #', '#          #', '############']
+
+    cartas = [carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, cartaJ, cartaQ, cartaK, cartaA]
+    qtcartas = 13
 
     for i in range(11):
         linha = ''
@@ -19,8 +55,8 @@ def imprimecartas():
                 linha += cartas[j][i] + '    '
         print(linha)
 
-
-imprimecartas()
+inicio()
+criarbaralho()
 
 
 
